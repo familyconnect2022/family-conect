6ef87627af421249298382d90aa79aa261e9c5b1
Thanhnhan1301 <huynh235@gmail.com>
2022-02-20 08:41:04 +0700

    commit -v2

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,23 +1,25 @@
 import {Layout} from 'antd'
 import {MenuOutlined, CloseOutlined} from '@ant-design/icons'
 import Image from 'next/image'
+import Link from 'next/link'
 export default function HeaderComponent({handleCollapsed, isCollapsed}) {
     const {Header} = Layout
     return (
         <Header className='flex justify-between items-center bg-green-100 z-20 px-5 lg:px-12'>
-            <div className='flex justify-center items-center gap-2 select-none '>
-                <div className='w-12 h-12 relative'>
-                    <Image
-                        src='/family-logo.png'
-                        layout='fill'
-                        alt='logo'
-                        priority={true}
-                    />
+            <Link href='/' passHref={true}>
+                <div className='flex justify-center items-center gap-2 select-none '>
+                    <div className='w-12 h-12 relative'>
+                        <Image
+                            src='/family-logo.png'
+                            layout='fill'
+                            alt='logo'
+                        />
+                    </div>
+                    <span className='text-[14px] font-semibold text-gray-700 italic uppercase'>
+                        Family & Connect
+                    </span>
                 </div>
-                <span className='text-[14px] font-semibold text-gray-700 italic uppercase'>
-                    Family & Connect
-                </span>
-            </div>
+            </Link>
             <div
                 className='
                     p-3 border bg-white 
--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,10 +3,24 @@ import {NextResponse} from 'next/server'
 
 export async function middleware(req) {
     const user = getCookie('user', {req})
+    const listIgnories = ['/auth', '/api', 'family-logo.png']
 
-    if (user) {
-        NextResponse.next()
+    const funcCheckIsIgnore = (path) => {
+        if (!listIgnories || listIgnories.length === 0) {
+            return true
+        } else {
+            return listIgnories.some((x) => {
+                return path.includes(x)
+            })
+        }
+    }
+
+    const isIgnore = funcCheckIsIgnore(req.url)
+    console.log(req.url, isIgnore)
+
+    if (user || isIgnore) {
+        return NextResponse.next()
     } else {
-        NextResponse.redirect('/auth/login')
+        return NextResponse.redirect('/auth/login')
     }
 }

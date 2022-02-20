248cbd297d2df707340b54182cb562969fd78460
Thanhnhan1301 <huynh235@gmail.com>
2022-02-20 08:58:14 +0700

    commit v3

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,25 +1,25 @@
-import {Layout} from 'antd'
-import {MenuOutlined, CloseOutlined} from '@ant-design/icons'
+import {CloseOutlined, MenuOutlined} from '@ant-design/icons'
 import Image from 'next/image'
 import Link from 'next/link'
 export default function HeaderComponent({handleCollapsed, isCollapsed}) {
-    const {Header} = Layout
     return (
-        <Header className='flex justify-between items-center bg-green-100 z-20 px-5 lg:px-12 cursor-pointer'>
-            <Link href='/' passHref={true}>
-                <div className='flex justify-center items-center gap-2 select-none '>
-                    <div className='w-12 h-12 relative'>
-                        <Image
-                            src='/family-logo.png'
-                            layout='fill'
-                            alt='logo'
-                        />
+        <div className='flex justify-between items-center w-full'>
+            <div className='cursor-pointer'>
+                <Link href='/' passHref={true}>
+                    <div className='flex justify-center items-center gap-2 select-none '>
+                        <div className='w-12 h-12 relative'>
+                            <Image
+                                src='/family-logo.png'
+                                layout='fill'
+                                alt='logo'
+                            />
+                        </div>
+                        <span className='text-[14px] font-semibold text-gray-700 italic uppercase'>
+                            Family & Connect
+                        </span>
                     </div>
-                    <span className='text-[14px] font-semibold text-gray-700 italic uppercase'>
-                        Family & Connect
-                    </span>
-                </div>
-            </Link>
+                </Link>
+            </div>
             <div
                 className='
                     p-3 border bg-white 
@@ -30,6 +30,6 @@ export default function HeaderComponent({handleCollapsed, isCollapsed}) {
             >
                 {isCollapsed ? <MenuOutlined /> : <CloseOutlined />}
             </div>
-        </Header>
+        </div>
     )
 }
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,14 +1,18 @@
 import {Layout} from 'antd'
 import {useCallback, useState} from 'react'
-import Header from './Header'
+import HeaderComponent from './Header'
 import Navbar from './Navbar'
 
 export default function LayoutComponents({children}) {
-    const {Content, Sider} = Layout
+    const {Content, Sider, Header} = Layout
     const [isCollapsed, setIsCollapsed] = useState(true)
     const handleCollapsed = () => setIsCollapsed(!isCollapsed)
     const ComponentContent = useCallback(() => {
-        return <Content className='w-full max-w-7xl m-auto'>{children}</Content>
+        return (
+            <Content className='w-full h-full min-h-screen max-w-7xl m-auto bg-red-100'>
+                {children}
+            </Content>
+        )
     }, [])
 
     return (
@@ -17,15 +21,17 @@ export default function LayoutComponents({children}) {
                 collapsedWidth={0}
                 collapsed={isCollapsed}
                 width='100vw'
-                className='fixed top-0 left-0 bottom-0 bg-white pt-[64px] lg:hidden'
+                className='fixed z-10 top-0 left-0 bottom-0 bg-white pt-[64px] lg:hidden'
             >
                 <Navbar />
             </Sider>
-            <Layout>
-                <Header
-                    handleCollapsed={handleCollapsed}
-                    isCollapsed={isCollapsed}
-                />
+            <Layout className=' w-full'>
+                <Header className='bg-green-100 z-20 px-5 lg:px-12'>
+                    <HeaderComponent
+                        handleCollapsed={handleCollapsed}
+                        isCollapsed={isCollapsed}
+                    />
+                </Header>
                 <ComponentContent />
             </Layout>
         </Layout>
